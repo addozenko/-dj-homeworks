57edf14e31ae4b6763606cd36c3dcad1d42ca05c
Andrey <andrei.dozenko@ya.ru>
2025-05-18 19:02:17 +0300

    Make hm_2.1_models_list_displ

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,29 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+import datetime
+from .models import Book
 
+
+def base(request):
+    template ='base.html'
+    return render(request, template)
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    context = {'books': Book.objects.all()}
     return render(request, template, context)
+
+
+def books_view_by_date(request, dt: datetime):
+    template = 'books/books_pagi.html'
+    books = Book.objects.filter(pub_date=dt)
+    paginator = Paginator(books, 3) 
+    page_number = request.GET.get('page') 
+    page = paginator.get_page(page_number)
+
+    context = {
+        'books': page.object_list,
+        'page': page  
+    }
+    return render(request, template, context)
+    
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, register_converter
+from books.converters import DateConverter
 
-from books.views import books_view
+from books.views import base, books_view, books_view_by_date
+
+register_converter(DateConverter, 'date')
+
 
 urlpatterns = [
-    path('', books_view, name='books'),
+    path('', base, name='index'),
     path('admin/', admin.site.urls),
+    path('books/', books_view, name='books'),
+    path('books/<date:dt>/', books_view_by_date, name='books_by_date')
 ]
