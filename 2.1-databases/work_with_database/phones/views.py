f1491ba22781d4f78ebb9195f87dd5374fb8edbf
Andrey <andrei.dozenko@ya.ru>
2025-05-18 11:27:17 +0300

    Create hm_2.1_work_wiht_db

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 
+from .models import Phone
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +8,17 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET['sort']
+    phones = {'name': Phone.objects.all().order_by('name'),
+              'min_price': Phone.objects.all().order_by('-price'),
+              'max_price' : Phone.objects.all().order_by('price'),}
+
+    context = {'phones': phones[sort]}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.all().get(slug = slug)
+    context = {'phone': phone}
     return render(request, template, context)
